842f6fde41e56556eb6408a85809bce626fcc03d
rey <3065588496@qq.com>
2023-06-01 21:24:17 +0800

    fixed <leader>h in rust file

--- lua/configs/rust-tools.lua
+++ lua/configs/rust-tools.lua
@@ -35,7 +35,7 @@ local on_attach = function(client, bufnr)
 
 	-- from rust tools
 	-- Hover actions
-	vim.keymap.set("n", "<leader>h", rt.hover_actions.hover_actions, { buffer = bufnr })
+	vim.keymap.set("n", "<leader>B", rt.hover_actions.hover_actions, { buffer = bufnr })
 	-- Code action groups
 	vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
 end
