310f2be278938909d501a6145a0f3302cd8d6de3
yongtenglei <3065588496@qq.com>
2023-07-01 00:22:28 +0100

    added latex stuff

--- lua/configs/masonlspconfig.lua
+++ lua/configs/masonlspconfig.lua
@@ -1,38 +1,38 @@
 local M = {}
 function M.config()
-	require("mason-lspconfig").setup({
-		-- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
-		-- This setting has no relation with the `automatic_installation` setting.
-		ensure_installed = {
-			"lua_ls",
-			-- use rustup instead install this directly
-			--"rust_analyzer",
-			"clangd",
-			"cmake",
-			"cssls",
-			"gopls",
-			"html",
-			"jsonls",
-			"quick_lint_js",
-			"ltex",
-			"marksman",
-			"pyright",
-			--"r_language_server",
-			"sqlls",
-			"taplo",
-			"vimls",
-			"vuels",
-			"yamlls",
-		},
-		-- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
-		-- This setting has no relation with the `ensure_installed` setting.
-		-- Can either be:
-		--   - false: Servers are not automatically installed.
-		--   - true: All servers set up via lspconfig are automatically installed.
-		--   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
-		--       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
-		automatic_installation = true,
-	})
+  require("mason-lspconfig").setup({
+    -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
+    -- This setting has no relation with the `automatic_installation` setting.
+    ensure_installed = {
+      "lua_ls",
+      -- use rustup instead install this directly
+      --"rust_analyzer",
+      "clangd",
+      "cmake",
+      "cssls",
+      "gopls",
+      "html",
+      "jsonls",
+      "quick_lint_js",
+      "texlab",
+      "marksman",
+      "pyright",
+      --"r_language_server",
+      "sqlls",
+      "taplo",
+      "vimls",
+      "vuels",
+      "yamlls",
+    },
+    -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
+    -- This setting has no relation with the `ensure_installed` setting.
+    -- Can either be:
+    --   - false: Servers are not automatically installed.
+    --   - true: All servers set up via lspconfig are automatically installed.
+    --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
+    --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+    automatic_installation = true,
+  })
 end
 
 return M
