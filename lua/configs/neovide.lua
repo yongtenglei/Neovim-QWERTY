d2153bf46ec45ce5169f65a2af6b816c9e4016c4
yongtenglei <3065588496@qq.com>
2023-08-08 23:48:31 +0100

    added neovide

---
+++ lua/configs/neovide.lua
@@ -0,0 +1,5 @@
+vim.o.guifont = "FiraCode Nerd Font Mono:h12"
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_cursor_animate_in_insert_mode = true
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_vfx_mode = "ripple"
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,3 +2,7 @@ require("core.settings")
 require("core.keymaps")
 
 require("core.plugins")
+
+if vim.g.neovide then
+	require("configs.neovide")
+end
