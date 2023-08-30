34706f712411a815dbc3e5ac97612cc816af3e03
yongtenglei <3065588496@qq.com>
2023-08-30 23:24:23 +0100

    refactored super compiler

--- lua/rey/core/settings.lua
+++ lua/rey/core/settings.lua
@@ -94,3 +94,58 @@ vim.api.nvim_create_autocmd({ "VimLeave" }, {
   pattern = { "*.tex" },
   command = "!texclear %",
 })
+
+-- auto change working directory
+-- vim.cmd([[autocmd BufEnter * silent! lcd %:p:h]])
+
+-- compiler
+vim.cmd([[noremap r :call CompileRunGcc()<CR>]])
+vim.cmd([[func! CompileRunGcc()
+  exec "w"
+  if &filetype == 'c'
+    exec "!g++ % -o %<"
+    exec "!time ./%<"
+  elseif &filetype == 'cpp'
+    set splitbelow
+    exec "!g++ -std=c++11 % -Wall -o %<"
+    :sp
+    :res -15
+    :term ./%<
+  elseif &filetype == 'java'
+    set splitbelow
+    :sp
+    :res -5
+    term javac % && time java %<
+  elseif &filetype == 'rust'
+    set splitbelow
+    :sp
+    :res -5
+    term cargo run
+  elseif &filetype == 'sh'
+    :!time bash %
+  elseif &filetype == 'python'
+    set splitbelow
+    :sp
+    :term python3 %
+  elseif &filetype == 'html'
+    silent! exec "!".g:mkdp_browser." % &"
+  elseif &filetype == 'markdown'
+    exec ":MarkdownPreview"
+  elseif &filetype == 'tex'
+    silent! exec "VimtexStop"
+    silent! exec "VimtexCompile"
+  elseif &filetype == 'dart'
+    exec "CocCommand flutter.run -d ".g:flutter_default_device." ".g:flutter_run_args
+    silent! exec "CocCommand flutter.dev.openDevLog"
+  elseif &filetype == 'javascript'
+    set splitbelow
+    :sp
+    :term export DEBUG="INFO,ERROR,WARNING"; node --trace-warnings .
+  elseif &filetype == 'go'
+    set splitbelow
+    :sp
+    :term go run .
+  elseif &filetype == 'plantuml'
+    exec "PlantumlOpen"
+  endif
+endfunc]])
