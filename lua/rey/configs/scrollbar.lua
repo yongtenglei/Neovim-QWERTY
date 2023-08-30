c1a0af70a6bfbc5f8e4ddb985d42d53b20dcd667
yongtenglei <3065588496@qq.com>
2023-08-31 00:15:02 +0100

    refactored scrollbar

--- lua/rey/configs/scrollbar.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-function M.config()
-	require("scrollbar").setup()
-end
-
-return M
---
+++ lua/rey/plugins/scrollbar.lua
@@ -0,0 +1,7 @@
+return {
+	"petertriho/nvim-scrollbar",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("scrollbar").setup()
+	end,
+}
