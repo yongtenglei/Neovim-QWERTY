cb8c7263093510582eb8501eeb13fe867a316637
yongtenglei <3065588496@qq.com>
2023-08-31 01:39:34 +0100

    refactored neovide

--- lua/rey/core/init.lua
+++ lua/rey/core/init.lua
@@ -1,6 +1,6 @@
 require("rey.core.settings")
 require("rey.core.keymaps")
 
--- if vim.g.neovide then
---  require("configs.neovide")
--- end
+if vim.g.neovide then
+	require("rey.core.neovide")
+end
