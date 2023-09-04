f95de64c7293cab30ac9d02a6b8961de3c2cfdb4
yongtenglei <yongtengrey@outlook.com>
2023-09-04 20:07:29 +0100

    changed nvim-ts-rainbow to rainbow-delimiters

--- lua/rey/plugins/treesitter.lua
+++ lua/rey/plugins/treesitter.lua
@@ -106,6 +106,31 @@ return {
 	end,
 	build = ":TSUpdate",
 	dependencies = {
-		{ "p00f/nvim-ts-rainbow" },
+		{
+			"HiPhish/rainbow-delimiters.nvim",
+			config = function()
+				local rainbow_delimiters = require("rainbow-delimiters")
+
+				vim.g.rainbow_delimiters = {
+					strategy = {
+						[""] = rainbow_delimiters.strategy["global"],
+						vim = rainbow_delimiters.strategy["local"],
+					},
+					query = {
+						[""] = "rainbow-delimiters",
+						lua = "rainbow-blocks",
+					},
+					highlight = {
+						"RainbowDelimiterRed",
+						"RainbowDelimiterYellow",
+						"RainbowDelimiterBlue",
+						"RainbowDelimiterOrange",
+						"RainbowDelimiterGreen",
+						"RainbowDelimiterViolet",
+						"RainbowDelimiterCyan",
+					},
+				}
+			end,
+		},
 	},
 }
