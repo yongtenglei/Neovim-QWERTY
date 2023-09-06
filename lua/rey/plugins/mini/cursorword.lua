48d4228d0810a4edf43b2352cab69bc76aa77038
yongtenglei <yongtengrey@outlook.com>
2023-09-06 01:24:35 +0100

    added mini.cursorword

---
+++ lua/rey/plugins/mini/cursorword.lua
@@ -0,0 +1,9 @@
+return {
+	"echasnovski/mini.cursorword",
+	event = { "BufReadPre", "BufNewFile" },
+	version = false,
+	config = function()
+		local mini_cursorword = require("mini.cursorword")
+		mini_cursorword.setup()
+	end,
+}
