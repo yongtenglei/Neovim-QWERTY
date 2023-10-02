af5ced2650d608682a8eb298de9c051b1f711ea8
yongtenglei <yongtengrey@outlook.com>
2023-10-02 22:28:56 +0100

    added im-select

---
+++ lua/rey/plugins/im_select.lua
@@ -0,0 +1,9 @@
+return {
+	-- should install im-select first, see
+	-- https://github.com/daipeihust/im-select
+	"keaising/im-select.nvim",
+	event = "InsertEnter",
+	config = function()
+		require("im_select").setup({})
+	end,
+}
