5d28a0ae3f80075473eb5d18481cbc8db264133a
yongtenglei <yongtengrey@outlook.com>
2023-10-21 00:33:10 +0100

    added mini.map

---
+++ lua/rey/plugins/mini/map.lua
@@ -0,0 +1,15 @@
+return {
+	"echasnovski/mini.map",
+	version = false,
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local mini_map = require("mini.map")
+		mini_map.setup()
+
+		-- open map automatically
+		mini_map.open()
+
+		local opts = { noremap = true, silent = true }
+		vim.keymap.set("n", "<leader>mm", "<cmd>lua MiniMap.toggle()<cr>", opts)
+	end,
+}
