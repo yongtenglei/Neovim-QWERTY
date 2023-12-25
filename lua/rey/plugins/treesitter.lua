466b8fd75f06725122fa009866fbfc78d45a8e68
yongtenglei <yongtengrey@outlook.com>
2023-12-25 14:52:52 +0000

    rainbow-delimiters.nvim version 0.3.0

--- lua/rey/plugins/treesitter.lua
+++ lua/rey/plugins/treesitter.lua
@@ -1,9 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   -- event = { "BufReadPre", "BufNewFile" },
-  --event = { "BufReadPre", "BufNewFile", "BufWinEnter" },
-  --event = { "BufWinEnter", "BufReadPre", "BufNewFile" },
-  --event = "BufWinEnter",
   config = function()
     require("nvim-treesitter.configs").setup({
       -- A list of parser names, or "all"
@@ -82,31 +79,6 @@ return {
         },
       },
 
-      rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        colors = {
-          "#cc241d",
-          "#a89984",
-          "#b16286",
-          "#d79921",
-          "#689d6a",
-          "#d65d0e",
-          "#458588",
-        }, -- table of hex strings
-        termcolors = {
-          "Red",
-          "Green",
-          "Yellow",
-          "Blue",
-          "Magenta",
-          "Cyan",
-          "White",
-        }, -- table of colour name strings
-      },
-
       autotag = {
         enable = true,
       },
@@ -124,10 +96,17 @@ return {
             [""] = rainbow_delimiters.strategy["global"],
             vim = rainbow_delimiters.strategy["local"],
           },
+
           query = {
             [""] = "rainbow-delimiters",
             lua = "rainbow-blocks",
           },
+
+          priority = {
+            [""] = 110,
+            lua = 210,
+          },
+
           highlight = {
             "RainbowDelimiterRed",
             "RainbowDelimiterYellow",
